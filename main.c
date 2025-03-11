ba5b5e790f2fb0099dc58085bb83797358824d57
Bryan Pinheiro <40837519+bryanpinheiro@users.noreply.github.com>
2025-03-11 10:11:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,69 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "pilhas.h"
 
 int main() {
 
-    pilha p;
+  pilha p;
 
-    stack_init(&p);
-    push(&p, 'A');
-    push(&p, 'B');
-    push(&p, 'C');
-    push(&p, 'D');
-    print_stack(&p);
+  stack_init(&p);
+  push(&p, 'A');
+  push(&p, 'B');
+  push(&p, 'C');
+  push(&p, 'D');
 
-    printf("\ninvert list:\n");
-    invert_stack(&p);
-    print_stack(&p);
+  printf("Topo antes: %d\n", p.topo);
+  t_dado removido;
+  pop(&p, &removido);
+  printf("Topo depois: %d\n", p.topo);
+  push(&p, 'E');
 
-    return 0;
+  printf("\nPilha inicial:\n");
+  print_stack(&p);
 
+  printf("\n0 - Inverter pilha:\n");
+  invert_stack(&p);
+  print_stack(&p);
+
+  printf("\n1 - Inverter string:\n");
+  invertString("Bryan");
+
+  printf("\n\n3 - Pilhas de ímpares e pares.\n");
+  pilha even, numbers;
+  stack_init(&even);
+  read_numbers_stack(&numbers);
+  get_evens_and_odds(&even, &numbers);
+  printf("\nPares: \n");
+  print_stack(&even);
+
+  printf("\n4 - Ler números e exibir na ordem:\n");
+  pilha pnumeros;
+  read_numbers_order(&pnumeros);
+
+  printf("\n5 - Ler números até ficar cheia:\n");
+  pilha pnumeros2;
+  read_numbers_stack(&pnumeros2);
+  print_stack(&pnumeros2);
+
+  printf("\n6 - Desempilhar pilhas:\n");
+  unstack(&p);
+
+  printf("\n7 - Comparar pilhas:\n");
+  pilha p2;
+  stack_init(&p2);
+  push(&p2, 'Z');
+  push(&p2, 'X');
+  push(&p2, 'Y');
+  push(&p2, 'O');
+  push(&p2, 'P');
+
+  bool isBigger = compare_stack(p, p2);
+  if (isBigger) {
+    printf("A pilha p1 é maior que a pilha p2");
+  } else {
+    printf("A pilha p2 é maior que a pilha p1");
+  }
+
+  return 0;
 }
-
