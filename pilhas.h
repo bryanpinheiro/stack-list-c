d2525a9d38afecdc8b09c2054fc0d330ce35077d
Bryan Pinheiro <40837519+bryanpinheiro@users.noreply.github.com>
2025-03-11 10:12:08 -0300

    Update pilhas.h

--- pilhas.h
+++ pilhas.h
@@ -4,8 +4,6 @@
 
 #include <stdbool.h>
 
-
-
 /** Tipo da Pilha (t_dado)
 
 ▼
@@ -25,46 +23,44 @@ typedef char t_dado;
 
 typedef struct {
 
-    t_dado itens[MAX_TAM];
+  t_dado itens[MAX_TAM];
 
-    int topo;
+  int topo;
 
 } pilha;
 
-
-
 ///: inicializa a pilha p no estado vazio
-
-void stack_init(pilha * p);
-
-
+void stack_init(pilha *p);
 
 ///: insere o elemento x no topo da pilha p
-
-bool push(pilha * p, t_dado x);
+bool push(pilha *p, t_dado x);
 
 ///: remove e devolve o elemento do topo da pilha p
-
-bool pop(pilha* p, t_dado* r);
+bool pop(pilha *p, t_dado *r);
 
 ///: acessa o elemento do topo da pilha p devolvendo uma cópia do seu valor
-
-bool top(pilha p, t_dado* topo);
+bool top(pilha p, t_dado *topo);
 
 ///: devolve verdadeiro se a pilha p estiver vazia e falso, caso contrário
-
 bool stack_isempty(pilha p);
 
 ///: devolve verdadeiro se a pilha p estiver cheia e falso, caso contrário
-
 bool stack_isfull(pilha p);
 
+void print_stack(pilha *p);
 
+void invert_stack(pilha *p);
 
-void print_stack(pilha * p);
+bool compare_stack(pilha p, pilha p2);
 
-void invert_stack(pilha * p);
+bool unstack(pilha *p);
 
+void read_numbers_stack(pilha *p);
+
+void read_numbers_order(pilha *p);
+
+void invertString(char *str);
+
+void get_evens_and_odds(pilha *even, pilha *numbers);
 
 #endif // _PILHAS_H_
-
